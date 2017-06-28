a2b4ab056423aa2a42a0b190a18aa8f197016c99
Administrator <beibei>
2017-06-28 17:54:54 +0800

    OKhttp简单demo over

--- app/src/main/java/com/example/okhttptest/MainActivity.java
+++ app/src/main/java/com/example/okhttptest/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.okhttptest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.send_request) {
+            sendRequestWithOkHttp();
+        }
+    }
+
+    private void sendRequestWithOkHttp() {
+        //        开启线程来发起网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client=new OkHttpClient();
+                    Request request=new Request.Builder().url("http://www.baidu.com").build();
+                    Response response=client.newCall(request).execute();
+                    String responseData=response.body().string();
+                    showResponse(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+//                在这里进行UI操作，将结果显示到界面上
+                responseText.setText(response);
+            }
+        });
     }
 }
